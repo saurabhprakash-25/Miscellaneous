bb563a8c72a2377e24f2971073e7e957744050be
Saurabh Prakash <saurabhprakash.smvit@gmail.com>
2021-01-15 12:12:33 +0530

    Write program to compute power (m,n)

---
+++ Recursion/power_of_m_raised_to_n.cpp
@@ -0,0 +1,25 @@
+// Uses tail recursin
+#include<iostream>
+
+long long power (int pNum, int pExponent, int pResult) {
+
+	if (pExponent == 0)
+		return pResult;
+
+	if (pExponent == 1)
+		return pResult * pNum;	
+	
+	return power(pNum, pExponent - 1, pResult * pNum);
+}
+
+int main (){
+
+	int m, n;
+
+	std::cin>>m;
+	std::cin>>n;
+
+	std::cout<<power (m, n, m);
+
+	return 0;
+}
