f5927d6b69af9edd7ff0f5fe50823f9d08e98195
Saurabh Prakash <saurabhprakash.smvit@gmail.com>
2021-03-30 23:05:40 +0530

    Convert indentation to spaces for Fibonacci program

--- Dynamic Programming/fibonacci.cpp
+++ Dynamic Programming/fibonacci.cpp
@@ -4,30 +4,30 @@ using namespace std;
 
 long long FindFibonacci(int pNum, vector<long long> & pMemo) {
 
-	if (pNum == 0)
-		return 0;
+    if (pNum == 0)
+        return 0;
 
-	if (pNum == 1)
-		return 1;
+    if (pNum == 1)
+        return 1;
 
-	if (pMemo[pNum - 1])
-		return pMemo[pNum - 1];
+    if (pMemo[pNum - 1])
+        return pMemo[pNum - 1];
 
-	pMemo[pNum -1] = FindFibonacci(pNum - 1, pMemo) + FindFibonacci(pNum - 2, pMemo);
+    pMemo[pNum -1] = FindFibonacci(pNum - 1, pMemo) + FindFibonacci(pNum - 2, pMemo);
 
-	return pMemo[pNum - 1];
+    return pMemo[pNum - 1];
 }
 
 int main()
 {
-	int				    n;
+    int	n;
 
     cout << "Which fibonacci number do you want me to find ? : ";
-	cin >> n;
+    cin >> n;
 
     vector<long long>   memo(n, 0);
 
-	cout << FindFibonacci(n, memo);
+    cout << FindFibonacci(n, memo);
 
-	return 0;
+    return 0;
 }
